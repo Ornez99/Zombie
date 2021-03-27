d6ceebe8e8a0e2e315759c840fa697897d46b971
Ornez99 <lukasz.chudy99@gmail.com>
2021-03-27 16:00:02 +0100

    Added FieldOfView Modified FogOfWar

--- Assets/Scripts/FogOfWar/FogOfWarAgent.cs
+++ Assets/Scripts/FogOfWar/FogOfWarAgent.cs
@@ -1,19 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class FogOfWarAgent : MonoBehaviour {
 
-    [SerializeField]
-    private float radius = default;
+
     private List<Node> nodesInRadius = new List<Node>();
+    private FieldOfViewAgent fieldOfViewAgent;
+
+    private void Awake() {
+        fieldOfViewAgent = GetComponent<FieldOfViewAgent>();
+    }
 
     private void Start() {
         FogOfWar.Instance.AddAgent(this);
     }
 
     public void UpdateNodesInRadius() {
-        nodesInRadius = Map.GetNodesInRadius(radius, Map.GetNodeFromPos(transform.position));   
+        nodesInRadius.Clear();
+        nodesInRadius.Add(Map.GetNodeFromPos(transform.position));
+        foreach (Vector3 endPos in fieldOfViewAgent.FieldOfViewVectors) {
+            float length = (endPos - transform.position).magnitude;
+            Vector3 direction = (endPos - transform.position).normalized;
+            float step = direction.magnitude;
+            float currentLength = step;
+            Vector3 currentPos = transform.position + direction;
+            while (currentLength <= length) {
+                Node candidate = Map.GetNodeFromPos(currentPos);
+                if (candidate != null && nodesInRadius.Contains(candidate) == false)
+                    nodesInRadius.Add(candidate);
+
+                currentPos += direction;
+                currentLength += step;
+            } 
+        }
     }
 
     public void SetNodesInRadius(bool visible) {
